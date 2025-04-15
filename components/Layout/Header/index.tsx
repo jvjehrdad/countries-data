f87148fe4d58fd22a06630a4c90762056dc6e9e9
Mehrdad <jvjehrdad@gmail.com>
2025-04-15 21:42:59 +0330

    file creation

---
+++ components/Layout/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return <header className={styles.container}>Header</header>;
+};
+
+export default Header;
